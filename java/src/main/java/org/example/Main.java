b3181eb25f7ed666e593b4926b2839d13fcf0359
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-14 23:10:21 -0500

    removed extra comments and unused imports

--- java/src/main/java/org/example/Main.java
+++ java/src/main/java/org/example/Main.java
@@ -1,12 +1,8 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         /* Reverse String */
